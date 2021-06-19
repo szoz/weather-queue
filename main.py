7f00643dec9b330d1c433798c2b8a337e413de12
Paweł Tymosiak <szozr2@gmail.com>
2021-06-19 13:17:44 +0200

    Added redis connection Added simple redis connection and 2 endpoints for reading data stored in redis

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-from flask import Flask, jsonify
+from flask import Flask, Response, request, jsonify, abort
 from flasgger import Swagger
+from redis import Redis
 
 app = Flask(__name__)
 swag = Swagger(app, template_file='docs/template.yml', config={"openapi": "3.0.2", "specs_route": "/docs/"}, merge=True)
 
+rdb = Redis()
+
 
 @app.route('/')
-def get_root():
+def get_root() -> Response:
     """Return simple greeting."""
     return jsonify({'hello': 'user'})
+
+
+@app.route('/redis')
+def redis_find_keys() -> Response:
+    """Return list of all Redis keys with given pattern."""
+    pattern = request.args.get('pattern', '*')
+    keys = [key.decode('utf-8') for key in rdb.keys(pattern)]
+    return jsonify(keys)
+
+
+@app.route('/redis/<string:key>')
+def redis_get_value(key: str) -> Response:
+    """Return value of given Redis key."""
+    value = rdb.get(key)
+    if not value:
+        return abort(404)
+
+    return jsonify({key: value.decode('utf-8')})
