14ec1d10565f3c0eb365ba63f45eb26385cc9527
Paweł Tymosiak <szozr2@gmail.com>
2021-06-19 12:46:21 +0200

    Added weather data collecting Added http client for collecting weather data from other API

---
+++ collect.py
@@ -0,0 +1,26 @@
+from requests import get
+from os import environ
+from typing import List, Union
+from time import sleep
+
+API_KEY = environ['API_KEY']
+API_URL = 'https://api.openweathermap.org/data/2.5/weather'
+
+
+def get_weather(city: str) -> Union[dict, None]:
+    """Return dict with current weather data for given city or None if city name is invalid."""
+    query_params = {'q': city, 'units': 'metric', 'appid': API_KEY}
+    response = get(API_URL, params=query_params)
+
+    if response.status_code == 200:
+        return response.json().get('main')
+
+
+def get_group_weather(cities: List[str]) -> dict:
+    """Return dict with current weather data for given city list."""
+    data = {}
+    for city in cities:
+        sleep(5)  # long processing simulation
+        data[city] = get_weather(city)
+
+    return data
